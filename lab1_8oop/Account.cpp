aa705e508e8b3a3daa5e119079f72ae33c7c85f0
DanVerh <danverh29@gmail.com>
2022-04-20 18:55:46 +0300

    2

--- lab1_8oop/Account.cpp
+++ lab1_8oop/Account.cpp
@@ -34,9 +34,57 @@ double Account::Money::Sum() {
     return 500 * am500 + 200 * am200 + 100 * am100 + 50 * am50 + 20 * am20 + 10 * am10 + 5 * am5 + 2 * am2 + am1 + 0.5 * am050 + 0.25 * am025 + 0.1 * am010 + 0.05 * am005 + 0.02 * am002 + 0.01 * am001;
 }
 
-Account::Money doubleMoney(double value) {
+Account::Money doubleMoney(double value)
+{
     Account::Money a;
-    a.Sum();
+
+    a.SetAm500((int)value / 500);
+    value -= (int)value / 500 > 0 ? 500 * (int)(value / 500) : 0;
+
+    a.SetAm200((int)value / 200);
+    value -= (int)value / 200 > 0 ? 200 * (int)(value / 200) : 0;
+
+    a.SetAm100((int)value / 100);
+    value -= (int)value / 100 > 0 ? 100 * (int)(value / 100) : 0;
+
+    a.SetAm50((int)value / 50);
+    value -= (int)value / 50 > 0 ? 50 * (int)(value / 50) : 0;
+
+    a.SetAm20((int)value / 20);
+    value -= (int)value / 20 > 0 ? 20 * (int)(value / 20) : 0;
+
+    a.SetAm10((int)value / 10);
+    value -= (int)value / 10 > 0 ? 10 * (int)(value / 10) : 0;
+
+    a.SetAm5((int)value / 5);
+    value -= (int)value / 5 > 0 ? 5 * (int)(value / 5) : 0;
+
+    a.SetAm2((int)value / 2);
+    value -= (int)value / 2 > 0 ? 2 * (int)(value / 2) : 0;
+
+    a.SetAm1((int)value / 1);
+    value -= (int)value / 1 > 0 ? 1 * (int)(value / 1) : 0;
+
+    value *= 100;
+
+    a.SetAm050((int)value / 50);
+    value -= (int)value / 50 > 0 ? 50 * (int)(value / 50) : 0;
+
+    a.SetAm025((int)value / 25);
+    value -= (int)value / 25 > 0 ? 25 * (int)(value / 25) : 0;
+
+    a.SetAm010((int)value / 10);
+    value -= (int)value / 10 > 0 ? 10 * (int)(value / 10) : 0;
+
+    a.SetAm005((int)value / 5);
+    value -= (int)value / 5 > 0 ? 5 * (int)(value / 5) : 0;
+
+    a.SetAm002((int)value / 2);
+    value -= (int)value / 2 > 0 ? 2 * (int)(value / 2) : 0;
+
+    a.SetAm001((int)value / 1);
+    value -= (int)value / 1 > 0 ? 1 * (int)(value / 1) : 0;
+
     return a;
 }
 
@@ -46,17 +94,17 @@ double SumSum(Account::Money a, Account::Money b)
 }
 
 double DiffSum(Account::Money a, Account::Money b) {
-    return a.Sum() + b.Sum();
+    return a.Sum() - b.Sum();
 }
 
 double ProdSum(Account::Money a, Account::Money b)
 {
-    return a.Sum() + b.Sum();
+    return a.Sum() * b.Sum();
 }
 
 double DivSum(Account::Money a, Account::Money b)
 {
-    return a.Sum() + b.Sum();
+    return a.Sum() / b.Sum();
 }
 
 Account::Money MultOnNum(Account::Money a, double value) {
@@ -164,8 +212,3 @@ string Account::toString() {
     d.replace(d.find("."), 1, ",");
     return d;
 }
-
-
-
-
-
--- lab1_8oop/Account.h
+++ lab1_8oop/Account.h
@@ -17,6 +17,38 @@ public:
         bool Init(int newam500, int newam200, int newam100, int newam50, int newam20, int newam10, int newam5, int newam2, int newam1, int newam050, int newam025, int newam010, int newam005, int newam002, int newam001);
         double Sum();
 
+        void SetAm500(const int am) { am500 = am; };
+        void SetAm200(const int am) { am200 = am; };
+        void SetAm100(const int am) { am100 = am; };
+        void SetAm50(const int am) { am50 = am; };
+        void SetAm20(const int am) { am20 = am; };
+        void SetAm10(const int am) { am10 = am; };
+        void SetAm5(const int am) { am5 = am; };
+        void SetAm2(const int am) { am2 = am; };
+        void SetAm1(const int am) { am1 = am; };
+        void SetAm050(const int am) { am050 = am; };
+        void SetAm025(const int am) { am025 = am; };
+        void SetAm010(const int am) { am010 = am; };
+        void SetAm005(const int am) { am005 = am; };
+        void SetAm002(const int am) { am002 = am; };
+        void SetAm001(const int am) { am001 = am; };
+
+        int GetAm500() const { return am500; };
+        int GetAm200() const { return am200; };
+        int GetAm100() const { return am100; };
+        int GetAm50() const { return am50; };
+        int GetAm20() const { return am20; };
+        int GetAm10() const { return am10; };
+        int GetAm5() const { return am5; };
+        int GetAm2() const { return am2; };
+        int GetAm1() const { return am1; };
+        int GetAm050() const { return am050; };
+        int GetAm025() const { return am025; };
+        int GetAm010() const { return am010; };
+        int GetAm005() const { return am005; };
+        int GetAm002() const { return am002; };
+        int GetAm001() const { return am001; };
+
         friend Money doubleMoney(double value);
         friend double SumSum(Money a, Money b);
         friend double DiffSum(Money a, Money b);
@@ -57,4 +89,3 @@ public:
 private:
     Money sum;
 };
-
